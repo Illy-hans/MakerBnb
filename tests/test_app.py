15ee0c208514ec1c313e7b3896979f7831ee0159
Reece Owen <reece_owen@hotmail.co.uk>
2024-01-11 17:11:26 +0000

    start on some tests for the index page. Tested index listings and rendering template.

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,6 +24,43 @@ def test_get_login(page, test_web_address):
     expect(h1_tag).to_have_text("MakersBnB")
 
 """
-
+When we call index.html, it renders the template
 """
 
+def test_get_index(page, test_web_address):
+    page.goto(f"http://{test_web_address}")
+    h1_tag = page.locator("h1")
+    expect(h1_tag).to_have_text("Welcome to MakersBnB")
+
+"""
+When we got to the index page, it shows all listings on the page
+"""
+def test_get_index_listings(page, test_web_address, db_connection):
+    db_connection.seed('seeds/makersbnb.sql')
+    page.goto(f"http://{test_web_address}")
+    list_items = page.locator(".image-container")
+    print(list_items)
+    expect(list_items).to_have_text(['\n\n\n'            
+                                    'Opulent Oak Haven£300 per night\n\n\n'            
+                                    'Stonegate Sanctuary£560 per night\n\n\n'
+                                    'Glass Vista Retreat£720 per night\n\n\n'            
+                                    'Remote Hillside Lodge£110 per night\n\n\n'            
+                                    'Alpine Oasis£150 per night\n\n\n'            
+                                    'Stone Serenity£340 per night\n\n\n'            
+                                    'Garden View Haven£80 per night\n\n'] )
+    
+"""
+When we click on sign in page, it redirects us to the correct page
+"""
+
+"""
+When click on create account, it shows us the create account page
+"""
+
+"""
+When user is signed in, it displays you are logged in
+"""
+
+"""
+When the user has signed out, it shows the the sign in and create account buttons
+"""
